3761b0688216dafd554b047f46d8be99394ed45f
MohamedFadelMoumeni <jackbadas01@gmail.com>
2022-04-26 16:14:24 +0200

    up

--- components/work-card/work-card.component.jsx
+++ components/work-card/work-card.component.jsx
@@ -1,14 +1,11 @@
 import {Container, ImageContainer, Content, Brand, Description} from "./work-card.styles"
 import Image from "next/image"
-import { Fade } from "react-reveal"
 
 const WorkCard = ({reverse, img, title, techStack}) => {
     return (
         <Container reverse={reverse}>
             <ImageContainer>
-                <Fade right={reverse} left={!reverse}>
                 <Image src={img} width="600" height="400"/>
-                </Fade>
             </ImageContainer>
             <Content >
                 <Brand>{title}</Brand>
