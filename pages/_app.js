8ec804a1f59378931d8a477689414984d06ffcda
MohamedFadelMoumeni <jackbadas01@gmail.com>
2022-04-29 02:50:03 +0000

    up

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,29 @@ import '../styles/globals.css'
 import { ThemeProvider } from 'styled-components'
 import theme from '../theme'
 import Script from 'next/script'
+import { useRouter } from 'next/router';
+import { useEffect } from "react"
 
 
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+  
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
    <>
     <Script id="google-analytics" strategy="afterInteractive">
