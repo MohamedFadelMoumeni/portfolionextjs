e08a41416f0f3e0d63416db3ffb2c1c9fff401e9
MohamedFadelMoumeni <jackbadas01@gmail.com>
2022-04-29 00:24:16 +0000

    up

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,33 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'styled-components'
 import theme from '../theme'
+import Head from 'next/head'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
+   <>
+    <Head>
+      <Script
+    strategy="lazyOnload"
+    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+  />
+
+  <Script id="google-analytics" strategy="lazyOnload">
+    {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+  </Script>
+    </Head>
     <ThemeProvider theme={theme}>
   <Component {...pageProps} />
   </ThemeProvider>
+   </>
   )
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Head, Main, NextScript } from 'next/document';
-import Script from "next/script"
 // Import styled components ServerStyleSheet
 import { ServerStyleSheet } from 'styled-components';
 
@@ -36,21 +35,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500&display=swap" rel="stylesheet"></link>
-<Script
-    strategy="lazyOnload"
-    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-  />
 
-  <Script id="google-analytics" strategy="lazyOnload">
-    {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-                `}
-  </Script>
         </Head>
         <body>
           <Main />
