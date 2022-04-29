4590eb5416293bb80a62f1bb3f5ea24157d46396
MohamedFadelMoumeni <jackbadas01@gmail.com>
2022-04-29 02:25:11 +0000

    up

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,19 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'styled-components'
 import theme from '../theme'
-import Script from 'next/script'
+import ReactGA from 'react-ga';
+
+
+if(typeof window !== "undefined"){
+  ReactGA.initialize('G-PMZSXKPHNH');
+ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 function MyApp({ Component, pageProps }) {
   return (
    <>
  
-      <Script
-    strategy="lazyOnload"
-    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-  />
-
-  <Script id="google-analytics" strategy="lazyOnload">
-    {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-                `}
-  </Script>
+    
    
     <ThemeProvider theme={theme}>
   <Component {...pageProps} />
